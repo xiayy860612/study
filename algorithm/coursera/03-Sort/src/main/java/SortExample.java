4dafd524a0c8a0dd94dc75fa14cbc092aa70755d
Amos Xia <xiayy860612@126.com>
2021-03-16 22:09:22 +0800

    implement basic sort

---
+++ algorithm/coursera/03-Sort/src/main/java/SortExample.java
@@ -0,0 +1,31 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public interface SortExample {
+    void sort(Comparable[] a);
+
+    default boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    default void exch(Comparable[] a, int i, int j) {
+        Comparable tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    default void show(Comparable[] a) {
+        for (int i = 0; i < a.length; ++i) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    default boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; ++i) {
+            if (less(a[i], a[i-1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
